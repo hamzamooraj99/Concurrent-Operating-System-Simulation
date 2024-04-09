60713619d1a5cc96aa18738bd99b763895bcd1bf
Hamza Mooraj <63708017+hamzamooraj99@users.noreply.github.com>
2024-04-09 14:09:15 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@
 public class Main {
 
 	public static void main(String[] args) {
-		//Used to provide selected testing (you should adapt this to your own tests)
+		//Used to provide selected testing
 			Tests exampleTests =  new Tests();
-			// exampleTests.ur1_example_test();
-			// exampleTests.ur2_test();
-			// exampleTests.ur3_example_test();
+			exampleTests.ur1_example_test();
+			exampleTests.ur2_test();
+			exampleTests.ur3_example_test();
 			exampleTests.ur4_example_test();
-			// exampleTests.ur5_test();
-			// exampleTests.ur5_mixed_test();
-			// exampleTests.ur6_test();
-			// exampleTests.ur6_mixed_test();
+			exampleTests.ur5_test();
+			exampleTests.ur5_mixed_test();
+			exampleTests.ur6_test();
+			exampleTests.ur6_mixed_test();
 		}
 	}
 
